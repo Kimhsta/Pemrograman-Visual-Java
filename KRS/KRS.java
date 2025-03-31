585ecff20cb9bb5a29b0f26763dd015467992093
Kiki Mahesta <kimsta.ti@gmail.com>
2025-03-31 19:59:32 +0700

    first commit

---
+++ KRS/KRS.java
@@ -0,0 +1,25 @@
+public class KRS {
+    public static void main(String[] args) {
+        System.out.println("\t\tKARTU RENCANA STUDI");
+        System.out.println("====================================================");
+        System.out.println("Nama\t\t: Eka Rizki Suwarno");
+        System.out.println("NIM\t\t: 230103161");
+        System.out.println("Kelas\t\t: TI-23A5");
+        System.out.println("Prog.Studi\t: S1 Teknik Informatika");
+        System.out.println("Semester\t: 4");
+        System.out.println("----------------------------------------------------");
+        System.out.println("Kode Mata Kuliah\tMata Kuliah\t\tSKS");
+        System.out.println("----------------------------------------------------");
+        System.out.println("TI20406\t\t\tPemrograman Visual\t3");
+        System.out.println("MKB312\t\t\tSistem Terdistribusi\t3");
+        System.out.println("TI100\t\t\tKeamanan Informasi\t3");
+        System.out.println("MKB317\t\t\tKecerdasan Buatan\t3");
+        System.out.println("MKB312\t\t\tPemrograman Mobile\t3");
+        System.out.println("TI20501\t\t\tMetode Nurmelik\t\t3");
+        System.out.println("MKK317\t\t\tMetodologi Penelitian\t2");
+        System.out.println("TI20505\t\t\tManajemen\t\t2");
+        System.out.println("----------------------------------------------------");
+        System.out.println("Total SKS\t\t\t\t\t22");
+        System.out.println("====================================================");
+    }
+}
---
+++ KTM/KTM.java
@@ -0,0 +1,13 @@
+public class KTM {
+    public static void main(String[] args) {
+        System.out.println("          KARTU TANDA MAHASISWA");
+        System.out.println("=========================================");
+        System.out.println("Nama \t\t: Eka Rizki Suwarno");
+        System.out.println("Alamat \t\t: Mojogedang, Karanganyar");
+        System.out.println("NIM \t\t: 230103161");
+        System.out.println("Prog.Studi \t: S1 Teknik Informatika");
+        System.out.println("Fakultas \t: FIKOM (Ilmu Komputer)");
+        System.out.println("=========================================");
+    }
+
+}
